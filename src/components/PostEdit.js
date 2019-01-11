4f22e9e089ed9724825da2740c30b804dcf227d5
Lucas Furlani rosa <lucas.furlani.rosa@gmail.com>
2019-01-10 23:21:49 -0200

    Managing local state when editing a post

--- src/components/PostEdit.js
+++ src/components/PostEdit.js
@@ -4,15 +4,58 @@ import { Link, withRouter } from 'react-router-dom'
 import { formatDate } from '../utils/helpers'
 
 class PostEdit extends Component {
+  state = {
+    title: '',
+    body: '',
+  }
+
+  handleTitleChange = (e) => {
+    const title = e.target.value
+
+    this.setState(() => ({
+      title
+    }))
+  }
+
+  handleBodyChange = (e) => {
+    const body = e.target.value
+
+    this.setState(() => ({
+      body
+    }))
+  }
+
+  componentDidMount() {
+    // Receiving values from the pros and setting to the local state of the component
+    this.setState(() => ({
+      title: this.props.post.title,
+      body: this.props.post.body
+    }))
+  }
+
   render() {
     const { post} = this.props
+    const { title, body } = this.state
 
     return (
       <div>
-        <input value={post.title}></input>
-          <h3 className='center'>{post.title}</h3>
-          <p><i>by {post.author} on {formatDate(post.timestamp)}</i></p>
-          <p>{post.body}</p>  
+        <p>Editing post <b>{post.title}</b> <i>by {post.author} on {formatDate(post.timestamp)}</i></p>
+        Title:
+        <p>
+          <input
+            placeholder="Post title"
+            value={title}
+            onChange={this.handleTitleChange}            
+          />
+        </p>
+        Content:
+        <p>
+          <textarea
+            placeholder="Post content"
+            value={body}
+            onChange={this.handleBodyChange}
+          />
+        </p>
       </div>
     )
   }
